105aaf6794e72c8617d73a2d2d1017750e2f464e
souravsharma110 <54199286+souravsharma110@users.noreply.github.com>
2020-02-07 11:35:13 +0530

    Added more MapmyIndia APIs

--- DemoApp/src/app/app.service.ts
+++ DemoApp/src/app/app.service.ts
@@ -15,6 +15,8 @@ export class DemoService {
   private securityURL: string = "https://outpost.mapmyindia.com/api/security/oauth/token?grant_type=" + this.grantType + "&client_id=" + this.clientId + "&client_secret=" + this.clientSecret;
   private autoSuggestURL: string = "https://atlas.mapmyindia.com/api/places/search/json?query=agra&location=28.5454,77.455454&bridge&explain&username=balmukand";
   private nearbyURL: string = "https://atlas.mapmyindia.com/api/places/nearby/json?explain&richData&username=balmukand&refLocation=28.467470,77.077518&keywords=FINATM";
+  private geocodeURL: string = "https://atlas.mapmyindia.com/api/places/geocode?address=mapmyindia 237 okhla phase 3";
+  private textsearchURL: string = "https://atlas.mapmyindia.com/api/places/textsearch/json?query=okhla phase 3&region=ind";
 
   getToken() {
     let promise = new Promise((resolve, reject) => {
@@ -59,6 +61,35 @@ export class DemoService {
     return promise;
   }
 
+  geocode(token: string) {
+    const _url = this.geocodeURL+"&access_token="+token;
+    const promise = new Promise((resolve, reject) => {
+      this.httpClient.get(_url)
+        .toPromise()
+        .then(
+            res => { // Success
+                resolve(res);
+            }
+        );
+    });
+    return promise;
+  }
+
+  textsearch(token: string) {
+    const _url = this.textsearchURL+"&access_token="+token;
+    const promise = new Promise((resolve, reject) => {
+      this.httpClient.get(_url)
+        .toPromise()
+        .then(
+            res => { // Success
+                resolve(res);
+            }
+        );
+    });
+    return promise;
+  }
+
+
   jsonp(uri) {
     return new Promise(function(resolve, reject) {
       const id = '_' + Math.round(10000 * Math.random());
@@ -79,4 +110,3 @@ export class DemoService {
   }
 
 }
-
